03d3091410d464a5fc9f75c68d44fc8bc093bd1e
Aamir Yasin <100421401+aamir910@users.noreply.github.com>
2025-01-16 11:58:25 +0500

    connected to the mongo db atlas

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+// Initialize Express app
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Basic Route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
